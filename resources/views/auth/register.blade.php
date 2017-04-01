4095771d837b6c914f2c2cfa8b23de2301f0c005
Jose Bermudez <nanographic.bermudez@gmail.com>
2017-03-31 20:15:37 -0400

    Editando Plantilla
    Agregando El Genero a la plantilla

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -24,15 +24,15 @@
                             </div>
                         </div>
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                        <div class="form-group{{ $errors->has('gender') ? ' has-error' : '' }}">
                             <label for="name" class="col-md-4 control-label">Name</label>
 
                             <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+                                <input id="gender" type="text" class="form-control" name="gender" value="{{ old('gender') }}" required autofocus>
 
-                                @if ($errors->has('name'))
+                                @if ($errors->has('gender'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
+                                        <strong>{{ $errors->first('gender') }}</strong>
                                     </span>
                                 @endif
                             </div>
